f261431330e242c90088ff951789cfccaf4c83c3
JasonLin <JasonLin1230@163.com>
2022-04-16 21:37:11 +0800

    Algorithm I

---
+++ Algorithms/StudyPlan/Algorithm/Algorithm I/116.java
@@ -0,0 +1,16 @@
+// 116. populating-next-right-pointers-in-each-node
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode level_start=root;
+        while(level_start!=null){
+            TreeLinkNode cur=level_start;
+            while(cur!=null){
+                if(cur.left!=null) cur.left.next=cur.right;
+                if(cur.right!=null && cur.next!=null) cur.right.next=cur.next.left;
+                
+                cur=cur.next;
+            }
+            level_start=level_start.left;
+        }
+    }
+}
---
+++ Algorithms/StudyPlan/Algorithm/Algorithm I/190.java
@@ -0,0 +1,12 @@
+// 190. Reverse Bits
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int sum = 0;
+        for(int i=0;i<32;i++) {
+            sum = (sum<<1) + (n&1);
+            n = n>>1;
+        }
+        return sum;
+    }
+}
